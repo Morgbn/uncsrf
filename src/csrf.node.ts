cc251470ee75c334dbd05f45d750aed7a9ffe1bd
Morgbn <morgan.balde@gmail.com>
2023-07-01 19:53:18 +0200

    feat: :sparkles: csrf for node & web

---
+++ src/csrf.node.ts
@@ -0,0 +1,71 @@
+import { createCipheriv, createDecipheriv, randomBytes } from "node:crypto";
+import type {
+  CipherCCMTypes,
+  CipherOCBTypes,
+  CipherGCMTypes,
+} from "node:crypto";
+
+export type EncryptAlgorithm =
+  | CipherCCMTypes
+  | CipherOCBTypes
+  | CipherGCMTypes
+  | string;
+export type EncryptSecret = Buffer;
+
+export const defaultEncryptAlgorithm: EncryptAlgorithm = "aes-256-cbc";
+
+export const importEncryptSecret = (
+  secret?: string,
+  _encryptAlgorithm?: EncryptAlgorithm // eslint-disable-line @typescript-eslint/no-unused-vars
+): Promise<EncryptSecret> => {
+  return Promise.resolve(
+    Buffer.from(secret ?? randomBytes(22).toString("base64"))
+  );
+};
+
+/**
+ * Create a new CSRF token (encrypt secret using csrfConfig.encryptAlgorithm)
+ */
+export const create = (
+  secret: string,
+  encryptSecret: EncryptSecret,
+  encryptAlgorithm: EncryptAlgorithm
+): Promise<string> => {
+  const iv = randomBytes(16);
+  const cipher = createCipheriv(
+    encryptAlgorithm,
+    Buffer.from(encryptSecret),
+    iv
+  );
+  const encrypted =
+    cipher.update(secret, "utf8", "base64") + cipher.final("base64");
+  return Promise.resolve(`${iv.toString("base64")}:${encrypted}`);
+};
+
+/**
+ * Check csrf token (decrypt secret using csrfConfig.encryptAlgorithm)
+ */
+export const verify = (
+  secret: string,
+  token: string,
+  encryptSecret: EncryptSecret,
+  encryptAlgorithm: EncryptAlgorithm
+): Promise<boolean> => {
+  const [iv, encrypted] = token.split(":");
+  if (!iv || !encrypted) {
+    return Promise.resolve(false);
+  }
+  let decrypted;
+  try {
+    const decipher = createDecipheriv(
+      encryptAlgorithm,
+      Buffer.from(encryptSecret),
+      Buffer.from(iv, "base64")
+    );
+    decrypted =
+      decipher.update(encrypted, "base64", "utf8") + decipher.final("utf8");
+  } catch {
+    return Promise.resolve(false);
+  }
+  return Promise.resolve(decrypted === secret);
+};
---
+++ src/csrf.web.ts
@@ -0,0 +1,87 @@
+const webCrypto = globalThis.crypto;
+const subtle: Crypto["subtle"] = webCrypto.subtle;
+const getRandomValues: Crypto["getRandomValues"] = (array: any) =>
+  webCrypto.getRandomValues(array);
+
+export type EncryptAlgorithm = "AES-CBC";
+export type EncryptSecret = JsonWebKey;
+
+export const defaultEncryptAlgorithm: EncryptAlgorithm = "AES-CBC";
+
+export const importEncryptSecret = async (
+  secret?: string,
+  encryptAlgorithm?: EncryptAlgorithm
+): Promise<EncryptSecret> => {
+  const encryptKey = await subtle.generateKey(
+    {
+      name: encryptAlgorithm ?? "AES-CBC",
+      length: 256,
+    },
+    true,
+    ["encrypt", "decrypt"]
+  );
+  return await subtle.exportKey("jwk", encryptKey);
+};
+
+const importKey = (key: JsonWebKey, encryptAlgorithm: EncryptAlgorithm) => {
+  return subtle.importKey(
+    "jwk",
+    key,
+    {
+      name: encryptAlgorithm,
+      length: 256,
+    },
+    true,
+    ["encrypt", "decrypt"]
+  );
+};
+
+/**
+ * Create a new CSRF token (encrypt secret using csrfConfig.encryptAlgorithm)
+ */
+export const create = async (
+  secret: string,
+  encryptSecret: EncryptSecret,
+  encryptAlgorithm: EncryptAlgorithm
+): Promise<string> => {
+  const iv = getRandomValues(new Uint8Array(16));
+  const encrypted = await subtle.encrypt(
+    { name: encryptAlgorithm, iv },
+    await importKey(encryptSecret, encryptAlgorithm),
+    new TextEncoder().encode(secret)
+  );
+  const encryptedBuffer = Buffer.from(new Uint8Array(encrypted));
+  return `${Buffer.from(iv).toString("base64")}:${encryptedBuffer.toString(
+    "base64"
+  )}`;
+};
+
+/**
+ * Check csrf token (decrypt secret using csrfConfig.encryptAlgorithm)
+ */
+export const verify = async (
+  secret: string,
+  token: string,
+  encryptSecret: EncryptSecret,
+  encryptAlgorithm: EncryptAlgorithm
+): Promise<boolean> => {
+  const [iv, encrypted] = token.split(":");
+  if (!iv || !encrypted) {
+    return false;
+  }
+  let decrypted;
+  try {
+    const encodedDecrypted = await subtle.decrypt(
+      {
+        name: encryptAlgorithm,
+        iv: Buffer.from(iv, "base64"),
+      },
+      await importKey(encryptSecret, encryptAlgorithm),
+      Buffer.from(encrypted, "base64")
+    );
+    decrypted = new TextDecoder().decode(encodedDecrypted);
+  } catch {
+    return false;
+  }
+  return decrypted === secret;
+};
